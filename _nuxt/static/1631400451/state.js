15093a7832899341fe96ffd96e87cefeb282530c
wmcheung <wmcheung@users.noreply.github.com>
2021-09-11 22:47:32 +0000

    deploy: 38a6cd27cddaeec78e5955f981303c363f0c93a8

---
+++ _nuxt/static/1631400451/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProposal-Reporting-Tool\u002F_nuxt\u002Fstatic\u002F1631400451",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FProposal-Reporting-Tool\u002F",assetsPath:"\u002FProposal-Reporting-Tool\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2904915"}}}}(null));
